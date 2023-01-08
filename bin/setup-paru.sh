eeff64495a60cbb6a05619f91dad4dacf28fbdcb
Reece Gilbert <reecerggil@gmail.com>
2023-01-09 11:21:36 +1300

    add git install in paru setup script

--- bin/setup-paru.sh
+++ bin/setup-paru.sh
@@ -2,7 +2,7 @@
 
 mkdir /tmp/paru-install
 cd /tmp/paru-install
-sudo pacman -S --needed base-devel
+sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
