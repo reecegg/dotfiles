11da8f15f6893136f0972ede0e3bb4fb22e4c581
Reece Gilbert <reece@luminate.one>
2024-07-29 17:57:20 +1200

    WIP nvim lua setup

--- .config/nvim/lua/config/plugins/thingllm-nvim.lua
+++ .config/nvim/lua/config/plugins/thingllm-nvim.lua
@@ -12,53 +12,77 @@ Only output valid code.
 Do not provide any backticks that surround the code.
 Never ever output backticks like this ```.
 Any comment that is asking you for something should be removed after you satisfy them.
-Other comments should left alone. Do not output backticks'
+Other comments should left alone.
+Do not output backticks!'
 ]]
+local code_temperature = 0.2
 
 local help_prompt = [[
 You are a helpful assistant.
 What I have sent are my notes so far.
 You are very curt, yet helpful.
 ]]
+local help_temperature = 0.7
 
--- The intention of this prompt to is to generate inline documentation for code. To make it well documented and easy to follow and understand.
+-- Inline Documentation Prompt
 local inline_documentation_prompt = [[
-You are a inline documentation generator assistant.
-Add comments to the code or text that will follow after the instructions.
-RFC 2119 and 8174 keywords are used to indicate requirements.
+You are an inline documentation generator assistant.
 
 Comment style:
 - Comments should be clear and concise.
 - Comments should indicate logical blocks of actions in code.
-- Comments should document intent behind the code, and what the code is trying to achieve, from the developers perspective.
-- Comments should not explain the code is doing at the code level.
-- Comments should not explain any code that is self-explanatory or obvious. Functions with only a couple lines of code don't need comments beyond the docblock.
-- Comments should use the most simple language reasonbily possible to explain the code.
-
-Docblock style:
-- Docblocks should be added to functions, classes, and variables that are not self-explanatory.
-- Docblocks should include the description, parameters, return values, and throwables. In that order.
-- Docblocks should have a short one line description. If the description is longer than one line, it should be split into a short and long description.
-- Docblocks should have an empty line between the description and semantic sections if its common convention for the language.
-- Docblocks should have an empty line between the short and long description if both are present.
-- Docblocks should be in the most common and supported format for the language.
+- Comments should document intent behind the code, and what the code is trying to achieve, from the 'business logic' perspective.
+- Comments should not explain what the code is doing at the code level, except where its purpose is not obvious.
+- Comments should not explain any code that is self-explanatory or obvious. Functions with only a couple of lines of code don't need documentation beyond the docblock description.
+- Comments should use the most simple language reasonably possible to explain the code.
 
 Instructions:
 - SHOULD add comments to the code to explain the intent behind the code.
-- SHOULD adjust formatting of the code to make it more readable, including adding empty lines between logical blocks.
-- SHOULD NOT docment what the code is doing at the code level.
-- MUST add the docblocks if they are missing.
+- SHOULD adjust the formatting of the code to make it more readable, including adding empty lines between logical blocks.
+- SHOULD NOT document what the code is doing at the code level.
 - MUST output only the modified input.
 - MUST NOT change the code logic.
 - MUST NOT change the code structure.
 - MUST NOT output any backticks for code blocks.
-- MUST NOT output any commentry in your response that is not inline documentation.
+- MUST NOT output any commentary in your response that is not inline documentation.
 - MUST NOT respond in markdown format.
 - MUST NOT output ``` for code blocks.
 - MUST NOT output ```lang for code blocks.
-]]
 
-local documentation_prompt_temperature = 0.2
+Core Instructions Repeated for Emphasis:
+- SHOULD add comments to the code to explain the intent behind the code.
+- MUST output only the modified input.
+
+Add comments to the code or text provided.
+]]
+local inline_documentation_temperature = 0.2
+
+-- Docblock Documentation Prompt
+local docblock_documentation_prompt = [[
+You are a docblock documentation generator assistant.
+
+Docblock style:
+- Docblocks should be added to functions, classes, members, and variables.
+- Docblocks should include a description, parameters, return values, and throwables. In that order. If applicable.
+- Docblocks should have a short description. If the description is long, it should be split into a short overview and long detailed description.
+- Docblocks should have an empty line between the description and semantic sections.
+- Docblocks should have an empty line between the short and long description if both are present.
+- Docblocks should be in the most common and supported format for the language.
+
+Instructions:
+- MUST add the docblocks if there are none.
+- MUST output only the modified input.
+- MUST NOT change the code logic.
+- MUST NOT change the code structure.
+- MUST NOT change the code comments.
+- MUST NOT respond in markdown format.
+- MUST NOT output any backticks for code blocks.
+- MUST NOT output ``` for code blocks.
+- MUST NOT output ```lang for code blocks.
+
+Add docblocks to provided code. Start the output with the docblock. If the docblock already exists, update it to match the code.
+]]
+local documentation_prompt_temperature = 0.8 -- High temperature is required to avoid skipping the docblock and going right to the function definition.
 
 return {
   enabled = true,
@@ -98,7 +122,7 @@ return {
           api_key_name = 'OPENAI_API_KEY',
           system_prompt = system_prompt,
           replace = true,
-          temperature = documentation_prompt_temperature,
+          temperature = code_temperature,
         },
         thingllm.make_openai_spec_curl_args,
         thingllm.handle_openai_spec_data
@@ -112,18 +136,33 @@ return {
           api_key_name = 'OPENAI_API_KEY',
           system_prompt = helpful_prompt,
           replace = false,
+          temperature = help_temperature,
         },
         thingllm.make_openai_spec_curl_args,
         thingllm.handle_openai_spec_data
       )
     end
-
     local function openai_inline_documentation()
       thingllm.invoke_llm_and_stream_into_editor({
           url = 'https://api.openai.com/v1/chat/completions',
           model = 'gpt-4o',
           api_key_name = 'OPENAI_API_KEY',
           system_prompt = inline_documentation_prompt,
+          replace = true,
+          temperature = inline_documentation_temperature,
+        },
+        thingllm.make_openai_spec_curl_args,
+        thingllm.handle_openai_spec_data
+      )
+    end
+
+    local function openai_docblock_documentation()
+      thingllm.invoke_llm_and_stream_into_editor({
+          url = 'https://api.openai.com/v1/chat/completions',
+          model = 'gpt-4o',
+          api_key_name = 'OPENAI_API_KEY',
+          system_prompt = docblock_documentation_prompt,
+          temperature = documentation_prompt_temperature,
           replace = true,
         },
         thingllm.make_openai_spec_curl_args,
@@ -153,10 +192,13 @@ return {
 
     -- vim.keymap.set({ 'n', 'v' }, '<leader>k', groq_replace, { desc = 'llm groq' })
     -- vim.keymap.set({ 'n', 'v' }, '<leader>K', groq_help, { desc = 'llm groq_help' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>lq', openai_help, { desc = 'llm openai_help' })
     vim.keymap.set({ 'n', 'v' }, '<leader>la', openai_help, { desc = 'llm openai_help' })
     vim.keymap.set({ 'n', 'v' }, '<leader>lr', openai_replace, { desc = 'llm openai_replace' })
     vim.keymap.set({ 'n', 'v' }, '<leader>ld', openai_inline_documentation, { desc = 'llm openai_inline_documentation' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>lb', openai_docblock_documentation, { desc = 'llm openai_docblock_generator' })
     -- vim.keymap.set({ 'n', 'v' }, '<leader>I', anthropic_help, { desc = 'llm anthropic_help' })
     -- vim.keymap.set({ 'n', 'v' }, '<leader>i', anthropic_replace, { desc = 'llm anthropic' })
+
   end,
 }
--- .config/nvim/lua/local-plugins/thingllm/thingllm.lua
+++ .config/nvim/lua/local-plugins/thingllm/thingllm.lua
@@ -8,8 +8,17 @@
 -- TODO: context includor.
 -- TODO: simple prompt template engine
 
+--- Main module table.
 local M = {}
+
+--- Import plenary job module.
 local Job = require 'plenary.job'
+
+--- Create a new autocommand group for ThingLLM that will be used to handle user escape action.
+local group = vim.api.nvim_create_augroup('THINGLLM__AutoGroup', { clear = true })
+
+-- Represents the currently active job in plugin.
+local active_job = nil
 
 --- Get the API from the environment variables.
 -- @param name The name of the environment variable to retrieve.
@@ -18,11 +27,15 @@ local function get_api_key(name)
   return os.getenv(name)
 end
 
---- Get all lines in the current buffer up to the cursor's position.
+---
+-- Retrieves all lines from the start of the buffer up to the cursor's current row.
 --
--- @return A string containing all the lines up to the cursor, concatenated by newline.
-function M.get_lines_until_cursor()
+-- This function collects the text content of the current buffer from the beginning
+-- up to the line where the cursor is positioned and concatenates it into a single string.
+--
+-- @return string The concatenated lines from the start of the buffer to the cursor's row.
 
+function M.get_lines_until_cursor()
   -- Get the current buffer and window.
   local current_buffer = vim.api.nvim_get_current_buf()
   local current_window = vim.api.nvim_get_current_win()
@@ -38,11 +51,13 @@ function M.get_lines_until_cursor()
   return table.concat(lines, '\n')
 end
 
-
+--- Get the visual selection in the current buffer.
 function M.get_visual_selection()
+  -- Get starting and ending positions for the visual selection
   local _, srow, scol = unpack(vim.fn.getpos 'v')
   local _, erow, ecol = unpack(vim.fn.getpos '.')
 
+  -- Handle line-wise visual mode
   if vim.fn.mode() == 'V' then
     if srow > erow then
       return vim.api.nvim_buf_get_lines(0, erow - 1, srow, true)
@@ -51,6 +66,7 @@ function M.get_visual_selection()
     end
   end
 
+  -- Handle character-wise visual mode
   if vim.fn.mode() == 'v' then
     if srow < erow or (srow == erow and scol <= ecol) then
       return vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {})
@@ -59,24 +75,43 @@ function M.get_visual_selection()
     end
   end
 
+  -- Handle block-wise visual mode
   if vim.fn.mode() == '\22' then
     local lines = {}
+
+    -- Normalize selection coordinates if necessary
     if srow > erow then
       srow, erow = erow, srow
     end
     if scol > ecol then
       scol, ecol = ecol, scol
     end
+
+    -- Collect text from each line in the block
     for i = srow, erow do
-      table.insert(lines, vim.api.nvim_buf_get_text(0, i - 1, math.min(scol - 1, ecol), i - 1, math.max(scol - 1, ecol), {})[1])
+      table.insert(lines,
+        vim.api.nvim_buf_get_text(0, i - 1, math.min(scol - 1, ecol), i - 1, math.max(scol - 1, ecol), {})[1])
     end
+
     return lines
   end
 end
 
+--- Generates curl arguments for making a request to Anthropic API.
+--
+-- Constructs the necessary curl arguments based on the provided options, user prompt, and system prompt.
+--
+-- @param opts table containing options like URL, API key name, and model.
+-- @param prompt string user input prompt.
+-- @param system_prompt string system level prompt.
+-- @return table curl arguments for the API request.
+-- @throw error if API key retrieval fails.
 function M.make_anthropic_spec_curl_args(opts, prompt, system_prompt)
+  -- Retrieve the URL and API key from the options
   local url = opts.url
   local api_key = opts.api_key_name and get_api_key(opts.api_key_name)
+
+  -- Prepare the data payload for the API request
   local data = {
     system = system_prompt,
     messages = { { role = 'user', content = prompt } },
@@ -84,99 +119,206 @@ function M.make_anthropic_spec_curl_args(opts, prompt, system_prompt)
     stream = true,
     max_tokens = 4096,
   }
+
+  -- Initialize the curl arguments
   local args = { '-N', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', vim.json.encode(data) }
+
+  -- Add API key to the headers if available
   if api_key then
     table.insert(args, '-H')
     table.insert(args, 'x-api-key: ' .. api_key)
     table.insert(args, '-H')
     table.insert(args, 'anthropic-version: 2023-06-01')
   end
+
+  -- Add the URL to the arguments
   table.insert(args, url)
+
   return args
 end
 
+--- Generates curl arguments for making a request to the OpenAI API.
+--
+-- This function prepares the necessary curl arguments, including headers and data payload,
+-- for making a POST request to the OpenAI API using the provided options, prompt, and system prompt.
+--
+-- @param opts table A table containing options such as URL, API key name, and model.
+-- @param prompt string The user prompt to be sent to the OpenAI API.
+-- @param system_prompt string The system prompt to be included in the request.
+-- @return table A list of curl arguments for the API request.
 function M.make_openai_spec_curl_args(opts, prompt, system_prompt)
+  -- Extract the URL and API key from the options
   local url = opts.url
   local api_key = opts.api_key_name and get_api_key(opts.api_key_name)
+  local temperature = opts.temperature or 0.7
+
+  -- Prepare the data payload for the API request
   local data = {
-    messages = { { role = 'system', content = system_prompt }, { role = 'user', content = prompt } },
+    messages = {
+      { role = 'system', content = system_prompt },
+      { role = 'user',   content = prompt }
+    },
     model = opts.model,
-    temperature = 0.7,
+    temperature = temperature,
     stream = true,
   }
+
+  -- Initialize curl arguments with necessary headers and data
   local args = { '-N', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', vim.json.encode(data) }
+
+  -- If an API key is provided, add it to the headers
   if api_key then
     table.insert(args, '-H')
     table.insert(args, 'Authorization: Bearer ' .. api_key)
   end
+
+  -- Add the URL to the curl arguments
   table.insert(args, url)
+
+  -- Return the complete list of curl arguments
   return args
 end
 
+--- Writes a string at the current cursor position.
+--
+-- This function schedules an insertion operation to run in the main loop of Neovim.
+-- It splits the input string by newline characters and inserts the lines at the current
+-- cursor position, preserving the undo history. Finally, it sets the cursor to the end
+-- of the inserted text.
+--
+-- @param str The string to be inserted at the cursor position.
 local function write_string_at_cursor(str)
+  -- Schedule the function to run in the main loop
   vim.schedule(function()
+    -- Get the current window
     local current_window = vim.api.nvim_get_current_win()
+
+    -- Get the current cursor position
     local cursor_position = vim.api.nvim_win_get_cursor(current_window)
     local row, col = cursor_position[1], cursor_position[2]
 
+    -- Split the input string by newline characters
     local lines = vim.split(str, '\n')
 
+    -- Join the undo history before inserting new lines
     vim.cmd("undojoin")
+
+    -- Insert the lines at the current cursor position
     vim.api.nvim_put(lines, 'c', true, true)
 
+    -- Calculate the new cursor position
     local num_lines = #lines
     local last_line_length = #lines[num_lines]
+
+    -- Set the cursor to the end of the inserted text
     vim.api.nvim_win_set_cursor(current_window, { row + num_lines - 1, col + last_line_length })
   end)
 end
 
+--- Generate a prompt based on visual selection or cursor position.
+-- If there is a visual selection, it retrieves the lines and optionally replaces them.
+-- If there is no visual selection, it gets the lines from the start to the cursor position.
+-- @param opts table: Options containing the 'replace' flag.
+-- @return string: The generated prompt.
 local function get_prompt(opts)
+  -- Retrieve the replace option and the visual selection of lines
   local replace = opts.replace
   local visual_lines = M.get_visual_selection()
   local prompt = ''
 
+  -- Check if there is a visual selection
   if visual_lines then
+    -- Concatenate the visual selection into a single string with newline separators
     prompt = table.concat(visual_lines, '\n')
+
     if replace then
+      -- Include the file name and range as context
+      local file_name = vim.fn.expand('%:t')
+      prompt = string.format("File: %s\n\n%s", file_name, prompt)
+
       vim.api.nvim_command 'normal! d'
-      vim.api.nvim_command 'normal! k'
+      -- open a new line above the cursor to make up for the deleted lines
+      vim.api.nvim_command 'normal! O'
     else
+      -- If replace is false, exit visual mode
       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', false, true, true), 'nx', false)
     end
   else
+    -- If no visual selection, get the lines from the start to the cursor position
     prompt = M.get_lines_until_cursor()
   end
 
   return prompt
 end
 
+--- Handles the Anthropics specification data by processing the data stream and event state.
+--- If the event state indicates a content block delta, it decodes the JSON data from the stream,
+--- logs it for debugging, and writes the text delta at the cursor position.
+---
+--- @param data_stream string The data stream containing JSON encoded information.
+--- @param event_state string The state of the event to determine how to handle the data.
+---
 function M.handle_anthropic_spec_data(data_stream, event_state)
+  -- Check if the event state indicates a content block delta
   if event_state == 'content_block_delta' then
+    -- Decode the JSON data from the data stream
     local json = vim.json.decode(data_stream)
+
+    -- If the JSON contains a text delta, write the text at the cursor position
     if json.delta and json.delta.text then
       write_string_at_cursor(json.delta.text)
+      return json.delta.text
     end
   end
 end
 
+---
+-- Handle the OpenAI specification data stream.
+--
+-- This function processes a JSON-encoded data stream from OpenAI, extracts the content
+-- within the 'delta' field, and writes it at the cursor position.
+--
+-- @param data_stream The JSON-encoded data stream to be processed.
+-- @return The extracted content if available.
+-- @throw If the JSON decoding fails or the expected structure is not found.
 function M.handle_openai_spec_data(data_stream)
+  -- Check if the data_stream contains the keyword "delta"
   if data_stream:match '"delta":' then
+    -- Decode the JSON data from the stream
     local json = vim.json.decode(data_stream)
+
+    -- Ensure the decoded JSON contains the necessary structure
     if json.choices and json.choices[1] and json.choices[1].delta then
+      -- Extract the content within the delta
       local content = json.choices[1].delta.content
       if content then
+        -- Write the extracted content at the cursor position
         write_string_at_cursor(content)
+        return content
       end
     end
   end
 end
 
-local group = vim.api.nvim_create_augroup('THINGLLM__AutoGroup', { clear = true })
-local active_job = nil
-
+--- Invokes a language model and streams the output into the editor.
+--
+-- This function sets up and manages a job to invoke a language model via a curl command.
+-- It streams the output back into the editor, handling data and events as they occur.
+--
+-- @param opts table The options for invoking the language model, including the system prompt.
+-- @param make_curl_args_fn function A function to generate curl arguments based on the provided options and prompts.
+-- @param handle_data_fn function A function to handle data received from the language model.
+--
+-- @return Job The active job that is streaming the language model output.
+-- @throws error Throws an error if the system prompt is missing.
 function M.invoke_llm_and_stream_into_editor(opts, make_curl_args_fn, handle_data_fn)
+  -- Clear any existing autocommands in the specified group
   vim.api.nvim_clear_autocmds { group = group }
+
+  -- Retrieve the prompt based on the provided options
   local prompt = get_prompt(opts)
+
+  -- Ensure the system prompt is provided
   if not opts.system_prompt or opts.system_prompt == "" then
     vim.api.nvim_err_writeln("System prompt is missing")
     return
@@ -184,28 +326,40 @@ function M.invoke_llm_and_stream_into_editor(opts, make_curl_args_fn, handle_dat
 
   local system_prompt = opts.system_prompt
 
+  -- Log the system prompt and user prompt for debugging
   nlog.log('system_prompt: ' .. system_prompt)
   nlog.log('prompt: ' .. prompt)
-  local args = make_curl_args_fn(opts, prompt, system_prompt)
-  local curr_event_state = nil
 
+  -- Generate curl arguments using the provided function
+  local args = make_curl_args_fn(opts, prompt, system_prompt)
+
+  local curr_event_state = nil
+  local output = {}
+
+  -- Function to parse the output line and handle events/data
   local function parse_and_call(line)
+    -- Check if the line contains an event and update the current event state
     local event = line:match '^event: (.+)$'
     if event then
       curr_event_state = event
       return
     end
+
+    -- Check if the line contains data and process it accordingly
     local data_match = line:match '^data: (.+)$'
     if data_match then
-      handle_data_fn(data_match, curr_event_state)
+      local content = handle_data_fn(data_match, curr_event_state)
+      table.insert(output, content)
     end
   end
 
+  -- Shutdown any active job before starting a new one
   if active_job then
     active_job:shutdown()
     active_job = nil
   end
 
+  -- Create a new job to run the curl command
   active_job = Job:new {
     command = 'curl',
     args = args,
@@ -215,11 +369,14 @@ function M.invoke_llm_and_stream_into_editor(opts, make_curl_args_fn, handle_dat
     on_stderr = function(_, _) end,
     on_exit = function()
       active_job = nil
+      nlog.log('LLM Output: ' .. table.concat(output, ''))
     end,
   }
 
+  -- Start the job
   active_job:start()
 
+  -- Create an autocommand to handle user escape action
   vim.api.nvim_create_autocmd('User', {
     group = group,
     pattern = 'THINGLLM_Escape',
@@ -228,11 +385,14 @@ function M.invoke_llm_and_stream_into_editor(opts, make_curl_args_fn, handle_dat
         active_job:shutdown()
         print 'LLM streaming cancelled'
         active_job = nil
+        nlog.log('LLM Output (cancelled): ' .. table.concat(output, ''))
       end
     end,
   })
 
+  -- Set a keymap to trigger the escape autocommand
   vim.api.nvim_set_keymap('n', '<Esc>', ':doautocmd User THINGLLM_Escape<CR>', { noremap = true, silent = true })
+
   return active_job
 end
 
