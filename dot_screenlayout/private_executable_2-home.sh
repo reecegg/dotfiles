91b6d634c7fa9969515d85cfb6bfa10f47307e82
Reece Gilbert <reece.gilbert@exaba.com>
2025-12-13 10:31:21 +1300

    misc screen layout updates

--- dot_screenlayout/private_executable_2-home.sh
+++ dot_screenlayout/private_executable_2-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --mode 1920x1080 --pos 3840x0 --rotate left --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output DVI-I-4-4 --off --output DVI-I-3-3 --off --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
