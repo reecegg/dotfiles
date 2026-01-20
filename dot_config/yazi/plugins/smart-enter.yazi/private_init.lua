ae74d9e911b0889e60c84f5cef3d037b948209f1
Reece Gilbert <reece.gilbert@exaba.com>
2026-01-21 11:15:04 +1300

    Add yazi smart-enter plugin for l/right to open files

---
+++ dot_config/yazi/plugins/smart-enter.yazi/private_init.lua
@@ -0,0 +1,12 @@
+return {
+    entry = function()
+        local h = cx.active.current.hovered
+        if h then
+            if h.cha.is_dir then
+                ya.manager_emit("enter", {})
+            else
+                ya.manager_emit("open", {})
+            end
+        end
+    end,
+}
