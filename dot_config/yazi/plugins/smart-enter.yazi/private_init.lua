2d02c764178c84914c8f17a63b6706661a2e94d6
Reece Gilbert <reece.gilbert@exaba.com>
2026-01-21 11:17:21 +1300

    Only open text-based files with l/right in yazi

--- dot_config/yazi/plugins/smart-enter.yazi/private_init.lua
+++ dot_config/yazi/plugins/smart-enter.yazi/private_init.lua
@@ -4,7 +4,15 @@ return {
         if h then
             if h.cha.is_dir then
                 ya.manager_emit("enter", {})
-            else
+            elseif h.mime and h.mime:find("^text/") or
+                   h.mime == "application/json" or
+                   h.mime == "application/javascript" or
+                   h.mime == "application/typescript" or
+                   h.mime == "application/x-sh" or
+                   h.mime == "application/xml" or
+                   h.mime == "application/toml" or
+                   h.mime == "application/x-yaml" or
+                   h.mime == "inode/x-empty" then
                 ya.manager_emit("open", {})
             end
         end
