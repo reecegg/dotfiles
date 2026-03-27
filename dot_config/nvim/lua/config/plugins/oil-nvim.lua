232714fdf33e4e3ff4140746ca53a76e4b352958
Reece Gilbert <reece.gilbert@exaba.com>
2026-03-27 17:38:39 +1300

    refresh oil buffers on focus and update spellfile

--- dot_config/nvim/lua/config/plugins/oil-nvim.lua
+++ dot_config/nvim/lua/config/plugins/oil-nvim.lua
@@ -6,5 +6,15 @@ return {
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     -- oil keybind to open the root of the project
     vim.keymap.set("n", "<leader>-", "<CMD>Oil .<CR>", { desc = "Open project root" })
+
+    -- Refresh oil buffers on focus to pick up external file changes
+    vim.api.nvim_create_autocmd("FocusGained", {
+      callback = function()
+        local view_ok, view = pcall(require, 'oil.view')
+        if view_ok and view.rerender_all_oil_buffers then
+          view.rerender_all_oil_buffers({ refetch = true })
+        end
+      end,
+    })
   end,
 }
